832f399f160caf228a3cbec9a02be3ce41110a21
pastibisa106 <35872007+pastibisa106@users.noreply.github.com>
2025-05-08 06:31:12 +0700

    Add files via upload

---
+++ LCD_Code.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+
+#define I2C_ADDR    0x3F // taruh address lcd agan kesini
+#define BACKLIGHT_PIN     3
+#define En_pin  2
+#define Rw_pin  1
+#define Rs_pin  0
+#define D4_pin  4
+#define D5_pin  5
+#define D6_pin  6
+#define D7_pin  7
+
+int n = 1;
+
+LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
+
+void setup()
+{
+ lcd.begin (16,2); //  <<----- My LCD was 16x2
+
+// Switch on the backlight
+lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
+lcd.setBacklight(HIGH);
+lcd.home (); // go home (baris pertama)
+
+ lcd.print("Baris pertama");  
+}
+
+void loop()
+{
+ lcd.setCursor (0,1);        //baris ke dua
+ lcd.print("Baris ke dua");
+ delay(3000);
+}
